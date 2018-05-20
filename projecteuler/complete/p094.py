c47bae549dbb31e577289683c6d4a7219f521444
vasiapatov <vasiapatov@gmail.com>
2018-05-20 13:37:58 -0400

    Finished p094, with a lot of help from OEIS. While trying brute force solution, bumped into a lot of precision issues that I didnt quite to the bottom of :/

---
+++ projecteuler/complete/p094.py
@@ -0,0 +1,54 @@
+"""
+ https://projecteuler.net/problem=94
+ Almost equilateral triangles
+
+ It is easily proved that no equilateral triangle exists with integral length sides and integral area. However, the almost equilateral triangle 5-5-6 has an area of 12 square units. 
+ We shall define an almost equilateral triangle to be a triangle for which two sides are equal and the third differs by no more than one unit. 
+ Find the sum of the perimeters of all almost equilateral triangles with integral side lengths and area and whose perimeters do not exceed one billion (1,000,000,000).
+"""
+import time
+import numpy as np
+import math
+startTime = time.clock()
+
+# recurrence relation that generates sides of the triangles
+# https://oeis.org/A120893
+def a(n):
+  if n == 0 or n == 1:
+    return 1
+  if n == 2:
+    return 5
+  return 3*a(n-1) + 3*a(n-2) - a(n-3)
+
+
+def calc1(a):
+  return math.sqrt((a + 1) * (3*a - 1)) * (a-1) / 4
+
+def calc2(a):
+  return math.sqrt((a - 1) * (3*a + 1)) * (a+1) / 4
+
+
+target = 1e9
+sum_perimeters = 0
+for i in range(2,20):
+
+  side = a(i)
+  a1,a2 = calc1(side), calc2(side)
+
+  if (abs(a1 - int(a1)) < 0.0000001):
+    perimeter = side + side + side - 1
+    #print('{:15d}{:15d}{:15d}{:15d}{:25.3f}'.format(side,side,side-1,perimeter,a1))
+  
+  elif (abs(a2 - int(a2)) < 0.0000001):
+    perimeter = side + side + side + 1
+    #print('{:15d}{:15d}{:15d}{:15d}{:25.3f}'.format(side,side,side+1,perimeter,a2))
+ 
+  if perimeter > target:
+    break
+  sum_perimeters += perimeter
+  
+print(sum_perimeters)
+
+
+endTime = time.clock()
+print('Time elapsed:', '{:0.6f}'.format(endTime-startTime), 'seconds.')
