9830507ddce047b86b7689ee52cf4dd17ca173ba
vasiapatov <vasiapatov@gmail.com>
2018-05-29 13:07:41 -0400

    just finished problem 91, it was easier than I thought. Four-level nested for loops and calculation of dot product to find right angles... easy as that. It runs in about 30 seconds, I'm positive I could have optimized it more. If I rewrote it in C I think it would take less than a second to run. Will try doing that later

--- projecteuler/complete/p091.py
+++ projecteuler/complete/p091.py
@@ -15,8 +15,32 @@
  Given that 0 x _1 , y _1 , x _2 , y _2
 """
 import time
+from functools import reduce
+from operator import mul
 startTime = time.clock()
 
-#code
+def dot(it1,it2):
+  return sum([reduce(mul,nums) for nums in zip(it1,it2)])
+
+
+count = 0
+limit = 51
+for x1 in range(0,limit):
+  for y1 in range(0,limit):
+    if ((x1,y1) == (0,0)):
+      continue
+    for x2 in range(0,limit):
+      for y2 in range(0,limit):
+        if((x2,y2) in [(0,0),(x1,y1)]):
+          continue
+    
+        vecs = [( (x1,y1),(x2,y2) ), ( (-x1,-y1),(x2-x1,y2-y1) ), ( (-x2,-y2),(x1-x2,y1-y2) )]
+  
+        for vec in vecs:
+          if not dot(vec[0],vec[1]):
+            count += 1
+            break
+
+print(count // 2)
 endTime = time.clock()
 print('Time elapsed:', '{:0.6f}'.format(endTime-startTime), 'seconds.')
