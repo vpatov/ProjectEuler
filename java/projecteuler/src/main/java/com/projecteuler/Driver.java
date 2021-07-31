eb633bc9fa9a2aaa9448f9f4e6893d937283ddb3
Vasia Patov <vasiapatov@gmail.com>
2021-07-31 02:36:50 -0400

    Add java directory

---
+++ java/projecteuler/src/main/java/com/projecteuler/Driver.java
@@ -0,0 +1,10 @@
+package com.projecteuler;
+
+public class Driver 
+{
+    public static void main( String[] args )
+    {
+        Problem002 p = new Problem002();
+        System.out.println( p.solution());
+    }
+}
